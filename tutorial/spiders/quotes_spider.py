97639e0a179eb4bce5177b1f85796d6da6320081
Manideep kumar Manda <50362585+Manideep9800@users.noreply.github.com>
2021-03-30 10:09:57 +1100

    Update quotes_spider.py

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -2,16 +2,17 @@ import scrapy
 
 
 class ToScrapeSpiderXPath(scrapy.Spider):
-    name = 'catalouge'
+    	name = 'catalouge'
 	allowed_domains= ['dmoztools.net']
-    start_urls = ['http://dmoztools.net/Arts/Animation/']
+    	start_urls = ['http://dmoztools.net/Arts/Animation/']
+	rules = [Rule(LinkExtractor(), callback='parse_item', follow=True)]
 			
     def parse(self, response):
         for title in response.xpath('//*[@id="site-item"]'):
             yield {
 				#'content': title.xpath('./div[@class="site-descr "]/text()').extract_first(),
 				'content': title.xpath('./div[@class="title-and-desc"]/div[@class="site-descr"]/text()').extract_first(),
-				#'title':title.path('./div[@class="title-and-desc"]/a/div[@class="site-title"]/text()').extract_first(),
-				#'url':title.path('./div[@class="title-and-desc"]/a/href').extract_first()
+				'title':title.path('./div[@class="title-and-desc"]/a/div[@class="site-title"]/text()').extract_first(),
+				'url':title.path('./div[@class="title-and-desc"]/a/href').extract_first()
 			}
 
